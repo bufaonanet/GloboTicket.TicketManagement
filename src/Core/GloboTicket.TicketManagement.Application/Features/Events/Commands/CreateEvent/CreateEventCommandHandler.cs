4d7fce69a77097c1347d293f43aa8f90766d5f25
bufaonanet <douglas.bufaonanet@outlook.com>
2022-06-06 12:02:35 -0300

    Adicionando camada de persistencia e infraestrutura

--- src/Core/GloboTicket.TicketManagement.Application/Features/Events/Commands/CreateEvent/CreateEventCommandHandler.cs
+++ src/Core/GloboTicket.TicketManagement.Application/Features/Events/Commands/CreateEvent/CreateEventCommandHandler.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
+using GloboTicket.TicketManagement.Application.Contracts.Infrastructure;
 using GloboTicket.TicketManagement.Application.Contracts.Persistence;
+using GloboTicket.TicketManagement.Application.Models.Mail;
 using GloboTicket.TicketManagement.Domain;
 using MediatR;
 using System;
@@ -12,12 +14,15 @@ namespace GloboTicket.TicketManagement.Application.Features.Events.Commands.Crea
     {
         private readonly IEventRepository _eventRepository;
         private readonly IMapper _mapper;
+        private readonly IEmailService _emailService;
 
-        public CreateEventCommandHandler(IEventRepository eventRepository, 
-                                         IMapper mapper)
+        public CreateEventCommandHandler(IEventRepository eventRepository,
+                                         IMapper mapper,
+                                         IEmailService emailService)
         {
             _eventRepository = eventRepository;
             _mapper = mapper;
+            _emailService = emailService;
         }
 
         public async Task<Guid> Handle(CreateEventCommand request, CancellationToken cancellationToken)
@@ -25,7 +30,7 @@ namespace GloboTicket.TicketManagement.Application.Features.Events.Commands.Crea
             var validator = new CreateEventCommandValidator(_eventRepository);
             var validationResult = await validator.ValidateAsync(request);
 
-            if(validationResult.Errors.Count > 0)
+            if (validationResult.Errors.Count > 0)
             {
                 throw new Exceptions.ValidationException(validationResult);
             }
@@ -33,6 +38,23 @@ namespace GloboTicket.TicketManagement.Application.Features.Events.Commands.Crea
             var @event = _mapper.Map<Event>(request);
 
             @event = await _eventRepository.AddAsync(@event);
+
+            var email = new Email
+            {
+                To = "douglas.bufaonanet@outlook.com",
+                Body = $"A new event was created: {request}",
+                Subject = "A new event was created"
+            };
+
+            try
+            {
+                await _emailService.SendEmail(email);
+            }
+            catch (Exception ex)
+            {
+                //this shouldn't stop the API from doing else so this can be logged
+                //_logger.LogError($"Mailing about event {@event.EventId} failed due to an error with the mail service: {ex.Message}");
+            }
 
             return @event.EventId;
         }
---
+++ src/Infrastructure/GloboTicket.TicketManagement.Persistence/Repositories/EventRepository.cs
@@ -0,0 +1,24 @@
+﻿using GloboTicket.TicketManagement.Application.Contracts.Persistence;
+using GloboTicket.TicketManagement.Domain;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GloboTicket.TicketManagement.Persistence.Repositories
+{
+    public class EventRepository : BaseRepository<Event>, IEventRepository
+    {
+
+        public EventRepository(GloboTicketDbContext dbContext)
+            : base(dbContext)
+        {
+        }
+
+        public Task<bool> IsEventNameAndDateUnique(string name, DateTime eventDate)
+        {
+            var matches = _dbContext.Events
+                .Any(e => e.Name.Equals(name) && e.Date.Date.Equals(eventDate.Date));
+            return Task.FromResult(matches);
+        }
+    }
+}
